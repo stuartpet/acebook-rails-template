e152ff66554141c4ee03e01883308ebf3f833786
Jordan Roberts <jordanroberts@talk21.com>
2019-05-23 15:33:12 +0100

    Testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  # validates :name, presence: true,
-  #                  length: { maximum: 25 }
-  # validates :email, presence: true,
-  #                   length: { maximum: 49 },
-  #                   uniqueness: true
-  #
-  # validates :password, length: { maximum: 29 }
+  validates :name, presence: true,
+                   length: { maximum: 25 }
+  validates :email, presence: true,
+                    length: { maximum: 49 },
+                    uniqueness: true
+
+  validates :password, length: { maximum: 29 }
 end
