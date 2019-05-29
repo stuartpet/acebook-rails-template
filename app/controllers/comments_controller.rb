aaf20901653f968e30ca4db990a0951a83364604
Kiah Hewitt <hewittkiah@gmail.com>
2019-05-29 16:14:47 +0100

    added post comment functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      flash[:success] = "Comment added"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = "Something went wrong!"
+      render root_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_many :posts
-  
+  has_many :comments, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # validates :name, presence: true,
