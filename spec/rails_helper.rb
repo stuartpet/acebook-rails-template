354360e20f753a69e1e67221e76d0a66a497ebbc
Stuart <stuart.pethurst@gmail.com>
2019-05-30 11:47:22 +0100

    driver fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,13 @@ require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
+
 Capybara.register_driver :selenium_marionette do |app|
  Capybara::Selenium::Driver.new(app, browser: :firefox, marionette: true)
 end
